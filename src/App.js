7cb67dba01bd8c741b25c0982c0e3188d2720bcf
Pablo <pablodplucenahh@gmail.com>
2024-09-02 17:22:28 -0300

    form

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Benefits />
       <Promo />
       <Testimonials />
-      {/* <SignupForm /> */}
+      <SignupForm />
     </div>
   );
 }
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -3,7 +3,7 @@ import './SignupForm.css';
 
 const SignupForm = () => {
     return (
-        <section className="signup-form" id="signup">
+        <section className="signup-form" id="signup" style={{ display: 'none' }}>
             <h2>Garanta Sua Aula Grátis</h2>
             <form name="contact" method="POST" data-netlify="true">
                 <p>
@@ -22,24 +22,3 @@ const SignupForm = () => {
 }
 
 export default SignupForm;
-
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
