83648f6bf6577ba7419335f6452095b49034c246
Pablo <pablodplucenahh@gmail.com>
2024-09-01 23:56:01 -0300

    form

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -5,7 +5,7 @@ const SignupForm = () => {
     return (
         <section className="signup-form" id="signup">
             <h2>Garanta Sua Aula Grátis</h2>
-            <form name="contact" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true">
                 <input type="text" name="nome" placeholder="Nome" required />
                 <input type="email" name="email" placeholder="E-mail" required />
                 <input type="tel" name="telefone" placeholder="Telefone" required />
