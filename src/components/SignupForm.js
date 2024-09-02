1927f866d19a21c1ee2b2b33baafe72fc0e789d1
Pablo <pablodplucenahh@gmail.com>
2024-09-02 00:36:11 -0300

    form_bonito

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -6,11 +6,15 @@ const SignupForm = () => {
         <section className="signup-form" id="signup">
             <h2>Garanta Sua Aula Grátis</h2>
             <form name="contact" method="POST" data-netlify="true">
-                <div>
-                    <input type="text" name="nome" placeholder="Nome" required />
-                    <input type="email" name="email" placeholder="E-mail" required />
-                    <input type="tel" name="telefone" placeholder="Telefone" required />
-                </div>
+                <p>
+                    <label><input type="text" name="nome" placeholder="Nome" required /></label>
+                </p>
+                <p>
+                    <label><input type="email" name="email" placeholder="E-mail" required /></label>
+                </p>
+                <p>
+                    <label><input type="tel" name="telefone" placeholder="Telefone" required /></label>
+                </p>
                 <button type="submit">Garantir Minha Aula Grátis</button>
             </form>
         </section>
@@ -18,3 +22,24 @@ const SignupForm = () => {
 }
 
 export default SignupForm;
+
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
