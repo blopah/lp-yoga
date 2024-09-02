ce2e368a976abc68646b3b034b6844fabd86ddaa
Pablo <pablodplucenahh@gmail.com>
2024-09-02 17:15:30 -0300

    <section className="signup-form" id="signup">             <h2>Garanta Sua Aula Grátis</h2>             <form name="contact" method="POST" data-netlify="true">                 <p>                     <label><input type="text" name="nome" placeholder="Nome" required /></label>                 </p>                 <p>                     <label><input type="email" name="email" placeholder="E-mail" required /></label>                 </p>                 <p>                     <label><input type="tel" name="telefone" placeholder="Telefone" required /></label>                 </p>                 <button type="submit">Garantir Minha Aula Grátis</button>             </form>         </section>

--- src/index.js
+++ src/index.js
@@ -9,5 +9,20 @@ root.render(
   <React.StrictMode>
     <Main />  {/* Vídeo de background sendo renderizado primeiro */}
     <App />   {/* Conteúdo principal sobre o vídeo */}
+    <section className="signup-form" id="signup">
+            <h2>Garanta Sua Aula Grátis</h2>
+            <form name="contact" method="POST" data-netlify="true">
+                <p>
+                    <label><input type="text" name="nome" placeholder="Nome" required /></label>
+                </p>
+                <p>
+                    <label><input type="email" name="email" placeholder="E-mail" required /></label>
+                </p>
+                <p>
+                    <label><input type="tel" name="telefone" placeholder="Telefone" required /></label>
+                </p>
+                <button type="submit">Garantir Minha Aula Grátis</button>
+            </form>
+        </section>
   </React.StrictMode>
 );
